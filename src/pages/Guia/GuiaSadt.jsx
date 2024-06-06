636805bd61ec7329328a5e194c49576ea0556e46
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-06-06 18:28:42 -0300

    update 7 files

---
+++ src/pages/Guia/GuiaSadt.jsx
@@ -0,0 +1,68 @@
+import './GuiaSadt.css'
+
+export default function GuiaSadt({ data, func }) {
+
+    return (
+        <>
+            <button className="btn_backGuia" onClick={() => func.setGuiaActive(false)}>Voltar</button>
+            <img className='guiasadt' src="/guiassadt.png" alt="" style={{ background: '#fff' }} />
+            <div className="sadt__content">
+                <div className="logo__imageSadt">
+                <img src={`./plansLogo/${data.plan}.png`} alt='Logo do plano de saúde' />
+                </div>
+                <div className="contant__patient">
+                    <p>{data.planNumber}</p>
+                    <p>{data.name}</p>
+                </div>
+                <div className="content__doctor">
+                    <p>{data.doctor}</p>
+                    <p>CRM</p>
+                    <p>{data.crm}</p>
+                    <p>RJ</p>
+                    <p>225225</p>
+                </div>
+                <div className="content__data">
+                    <p>{data.dateSol.split('-').reverse().join('/')}</p>
+                    <p>{data.ind}</p>
+                </div>
+                <div className="content__proced">
+                    <p>{data.tuss1 && '22'}</p>
+                    <p>{data.tuss1}</p>
+                    <p>{data.proced1}</p>
+                    <p >{data.qtd1 === '' ? '' : data.qtd1?.toString().padStart(2, '0')}</p>
+                </div>
+                <div className="content__proced">
+                    <p>{data.tuss2 && '22'}</p>
+                    <p>{data.tuss2}</p>
+                    <p>{data.proced2}</p>
+                    <p >{data.qtd2 === '' ? '' : data.qtd2?.toString().padStart(2, '0')}</p>
+                </div>
+                <div className="content__proced">
+                    <p>{data.tuss3 && '22'}</p>
+                    <p>{data.tuss3}</p>
+                    <p>{data.proced3}</p>
+                    <p >{data.qtd3 === '' ? '' : data.qtd3?.toString().padStart(2, '0')}</p>
+                </div>
+                <div className="content__proced">
+                    <p>{data.tuss4 && '22'}</p>
+                    <p>{data.tuss4}</p>
+                    <p>{data.proced4}</p>
+                    <p >{data.qtd4 === '' ? '' : data.qtd4?.toString().padStart(2, '0')}</p>
+                </div>
+                <div className="content__proced">
+                    <p>{data.tuss5 && '22'}</p>
+                    <p >{data.tuss5}</p>
+                    <p >{data.proced5}</p>
+                    <p >{data.qtd5 === '' ? '' : data.qtd5?.toString().padStart(2, '0')}</p>
+                </div>
+
+            </div>
+
+            <div id="logoImage">
+                {/* <img id="logo" src={`./plansLogo/${data.plan}.png`} alt="logo do plano de saúde" /> */}
+            </div>
+
+        </>
+
+    )
+}
--- src/pages/Guias/Guias.jsx
+++ src/pages/Guias/Guias.jsx
@@ -7,10 +7,12 @@ import axios from 'axios';
 import tuss from '../../api/tabela_22';
 import Guia from "../Guia/Guia";
 import Header from "../../components/Shared/Header";
+import GuiaSadt from "../Guia/GuiaSadt";
 
 export default function Guias({ pageActive }) {
 
-    const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+    const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm();
+    const [typeGuia, setTypeGuia] = useState('int')
 
     const [tussField1, setTussField1] = useState(false)
     const [tussField2, setTussField2] = useState(false)
@@ -70,107 +72,194 @@ export default function Guias({ pageActive }) {
             <Header />
             <div className="content">
                 <h1 className='title'>Gerar Guias</h1>
-                <form className="formContainer-guia" onSubmit={handleSubmit(createGuia)}>
-                    <fieldset>
-                        <h3 className="formField-title">Dados do Paciente</h3>
-                        <label htmlFor=''>Nome do Paciente:</label>
-                        <input type='text' {...register("name")} />
-                        <label htmlFor=''>Nº da Carteirinha:</label>
-                        <input type='text' {...register("planNumber")} />
-                        <label htmlFor=''>Plano de Saúde:</label>
-                        <select {...register("plan")}>
-                            <option value="" selected disabled>Plano</option>
-                            <option value="amil">Amil</option>
-                            <option value="assim">Assim</option>
-                            <option value="caberj">Caberj</option>
-                            <option value="cassi">Cassi</option>
-                            <option value="eletros">Eletros Saúde</option>
-                            <option value="fiosaude">Fio Saúde</option>
-                            <option value="gama">Gama Saúde</option>
-                            <option value="goldencross">Golden Cross</option>
-                            <option value="intermedica">Intermédica</option>
-                            <option value="ipalerj">Ipalerj</option>
-                            <option value="petrobras">Petrobras</option>
-                            <option value="realgrandeza">Real Grandeza</option>
-                            <option value="caixa">Saúde Caixa</option>
-                            <option value="unimed">Unimed</option>
-                            <option value="vale">Vale</option>
-                        </select>
-                    </fieldset>
-                    <fieldset>
-                        <h3 className="formField-title">Dados da Autorização</h3>
-                        <label htmlFor=''>Nº da Guia:</label>
-                        <input type='text' {...register("guia")} />
-                        <label htmlFor=''>Nº da Senha:</label>
-                        <input type='text' {...register("password")} />
-                        <label htmlFor=''>Médico Solicitante:</label>
-                        <input type='text' {...register("doctor")} />
-                        <label htmlFor=''>CRM do Médico:</label>
-                        <input type='text' {...register("crm")} />
-                        <label htmlFor=''>Data da Solicitação:</label>
-                        <input type='date' {...register("dateSol")} />
-                        <label htmlFor=''>Data da Autorização:</label>
-                        <input type='date' {...register("dateAut")} />
-                        <div className="formField__data">
-                        </div>
-                    </fieldset>
-                    <fieldset>
-                        <h3 className="formField-title">Dados da Internação</h3>
-                        <label htmlFor=''>Tipo de Internação:</label>
-                        <select {...register("type")}>
-                            <option value="1">Clínica</option>
-                            <option value="2">Cirúrgica</option>
-                            <option value="4">Pediátrica</option>
-                            <option value="3">Ginecológica</option>
-                        </select>
-                        <label htmlFor=''>Caráter de Internação:</label>
-                        <select {...register("carater")}>
-                            <option value="1">Urgência</option>
-                            <option value="2">Eletiva</option>
-                        </select>
-                        <label htmlFor=''>Acomodação:</label>
-                        <select {...register("typeRoom")}>
-                            <option value="Apt">Quarto</option>
-                            <option value="Enf">Enfermaria</option>
-                            <option value="UTI">UTI</option>
-                        </select>
-                        <label htmlFor=''>Diárias:</label>
-                        <select {...register("daily")}>
-                            <option value="1" selected>1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </fieldset>
-                    <fieldset>
-                        <h3 className="formField-title">Procedimentos</h3>
-                        <div className="formField__content-tuss">
-                            <label htmlFor=''>Código TUSS:</label>
-                            <input type='text' {...register("tuss1")} onChange={(e) => searchTuss(e)} />
-                            <label htmlFor=''>Nome do Procedimento:</label>
-                            <input type='text' {...register("proced1")} />
-                            <label htmlFor='' >Quantidade:</label>
-                            <input type='text' defaultValue='1' {...register("qtd1")} />
-                        </div>
-                        <div className="formField__divider"></div>
-                        {tussField1 && inputTuss(2)}
-                        <div className="formField__divider"></div>
-                        {tussField2 && inputTuss(3)}
-                        <div className="formField__divider"></div>
-                        {tussField3 && inputTuss(4)}
-                        <div className="formField__divider"></div>
-                        {tussField4 && inputTuss(5)}
-                        <div className="formField__divider"></div>
-                        {tussField5 && inputTuss(6)}
-                        <label htmlFor=''>Adicionar Procedimento:</label>
-                        <button onClick={addTuss} type='button'>Adicionar</button>
-                        <button onClick={removeTuss} type='button'>Remover</button>
-                    </fieldset>
-                    {/* <button onClick={() => pageActive('guia')}>gerar</button> */}
-                    <button type="submit">Texto do botão</button>
-                </form>
-                {guiaActive &&
+                <select className="guias__typeGuia" onChange={(e) => setTypeGuia(e.target.value)}>
+                    <option value="int">Guia Internação</option>
+                    <option value="sadt">Guia Sadt</option>
+                </select>
+                {typeGuia === 'int' &&
+                    <form className="formContainer-guia" onSubmit={handleSubmit(createGuia)}>
+                        <fieldset>
+                            <h3 className="formField-title">Dados do Paciente</h3>
+                            <label htmlFor=''>Nome do Paciente:</label>
+                            <input type='text' {...register("name")} />
+                            <label htmlFor=''>Nº da Carteirinha:</label>
+                            <input type='text' {...register("planNumber")} />
+                            <label htmlFor=''>Plano de Saúde:</label>
+                            <select {...register("plan")}>
+                                <option value="" selected disabled>Plano</option>
+                                <option value="amil">Amil</option>
+                                <option value="assim">Assim</option>
+                                <option value="caberj">Caberj</option>
+                                <option value="cassi">Cassi</option>
+                                <option value="eletros">Eletros Saúde</option>
+                                <option value="fiosaude">Fio Saúde</option>
+                                <option value="gama">Gama Saúde</option>
+                                <option value="goldencross">Golden Cross</option>
+                                <option value="intermedica">Intermédica</option>
+                                <option value="ipalerj">Ipalerj</option>
+                                <option value="petrobras">Petrobras</option>
+                                <option value="realgrandeza">Real Grandeza</option>
+                                <option value="caixa">Saúde Caixa</option>
+                                <option value="unimed">Unimed</option>
+                                <option value="vale">Vale</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <h3 className="formField-title">Dados da Autorização</h3>
+                            <label htmlFor=''>Nº da Guia:</label>
+                            <input type='text' {...register("guia")} />
+                            <label htmlFor=''>Nº da Senha:</label>
+                            <input type='text' {...register("password")} />
+                            <label htmlFor=''>Médico Solicitante:</label>
+                            <input type='text' {...register("doctor")} />
+                            <label htmlFor=''>CRM do Médico:</label>
+                            <input type='text' {...register("crm")} />
+                            <label htmlFor=''>Data da Solicitação:</label>
+                            <input type='date' {...register("dateSol")} />
+                            <label htmlFor=''>Data da Autorização:</label>
+                            <input type='date' {...register("dateAut")} />
+                            <div className="formField__data">
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <h3 className="formField-title">Dados da Internação</h3>
+                            <label htmlFor=''>Tipo de Internação:</label>
+                            <select {...register("type")}>
+                                <option value="1">Clínica</option>
+                                <option value="2">Cirúrgica</option>
+                                <option value="4">Pediátrica</option>
+                                <option value="3">Ginecológica</option>
+                            </select>
+                            <label htmlFor=''>Caráter de Internação:</label>
+                            <select {...register("carater")}>
+                                <option value="1">Urgência</option>
+                                <option value="2">Eletiva</option>
+                            </select>
+                            <label htmlFor=''>Acomodação:</label>
+                            <select {...register("typeRoom")}>
+                                <option value="Apt">Quarto</option>
+                                <option value="Enf">Enfermaria</option>
+                                <option value="UTI">UTI</option>
+                            </select>
+                            <label htmlFor=''>Diárias:</label>
+                            <select {...register("daily")}>
+                                <option value="1" selected>1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <h3 className="formField-title">Procedimentos</h3>
+                            <div className="formField__content-tuss">
+                                <label htmlFor=''>Código TUSS:</label>
+                                <input type='text' {...register("tuss1")} onChange={(e) => searchTuss(e)} />
+                                <label htmlFor=''>Nome do Procedimento:</label>
+                                <input type='text' {...register("proced1")} />
+                                <label htmlFor='' >Quantidade:</label>
+                                <input type='text' {...register("qtd1")} />
+                            </div>
+                            <div className="formField__divider"></div>
+                            {tussField1 && inputTuss(2)}
+                            <div className="formField__divider"></div>
+                            {tussField2 && inputTuss(3)}
+                            <div className="formField__divider"></div>
+                            {tussField3 && inputTuss(4)}
+                            <div className="formField__divider"></div>
+                            {tussField4 && inputTuss(5)}
+                            <div className="formField__divider"></div>
+                            {tussField5 && inputTuss(6)}
+                            <label htmlFor=''>Adicionar Procedimento:</label>
+                            <button onClick={addTuss} type='button'>Adicionar</button>
+                            <button onClick={removeTuss} type='button'>Remover</button>
+                        </fieldset>
+                        {/* <button onClick={() => pageActive('guia')}>gerar</button> */}
+                        <button type="submit">Gerar Guia</button>
+                        <button type="button" onClick={() => reset()}>Limpar</button>
+                    </form>
+                }
+
+                {typeGuia === 'sadt' &&
+                    <form className="formContainer-guia" onSubmit={handleSubmit(createGuia)}>
+                        <fieldset>
+                            <h3 className="formField-title">Dados do Paciente</h3>
+                            <label htmlFor=''>Nome do Paciente:</label>
+                            <input type='text' {...register("name")} />
+                            <label htmlFor=''>Nº da Carteirinha:</label>
+                            <input type='text' {...register("planNumber")} />
+                            <label htmlFor=''>Plano de Saúde:</label>
+                            <select {...register("plan")}>
+                                <option value="" selected disabled>Plano</option>
+                                <option value="amil">Amil</option>
+                                <option value="assim">Assim</option>
+                                <option value="caberj">Caberj</option>
+                                <option value="cassi">Cassi</option>
+                                <option value="eletros">Eletros Saúde</option>
+                                <option value="fiosaude">Fio Saúde</option>
+                                <option value="gama">Gama Saúde</option>
+                                <option value="goldencross">Golden Cross</option>
+                                <option value="intermedica">Intermédica</option>
+                                <option value="ipalerj">Ipalerj</option>
+                                <option value="petrobras">Petrobras</option>
+                                <option value="realgrandeza">Real Grandeza</option>
+                                <option value="caixa">Saúde Caixa</option>
+                                <option value="unimed">Unimed</option>
+                                <option value="vale">Vale</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <h3 className="formField-title">Dados da Autorização</h3>
+                            {/* <label htmlFor=''>Nº da Guia:</label>
+                            <input type='text' {...register("guia")} /> */}
+                            {/* <label htmlFor=''>Nº da Senha:</label>
+                            <input type='text' {...register("password")} /> */}
+                            <label htmlFor=''>Médico Solicitante:</label>
+                            <input type='text' {...register("doctor")} />
+                            <label htmlFor=''>CRM do Médico:</label>
+                            <input type='text' {...register("crm")} />
+                            <label htmlFor=''>Data da Solicitação:</label>
+                            <input type='date' {...register("dateSol")} />
+                            {/* <label htmlFor=''>Data da Autorização:</label>
+                            <input type='date' {...register("dateAut")} /> */}
+                            <div className="formField__data">
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <h3 className="formField-title">Procedimentos</h3>
+                            <div className="formField__content-tuss">
+                                <label htmlFor=''>Código TUSS:</label>
+                                <input type='text' {...register("tuss1")} onChange={(e) => searchTuss(e)} />
+                                <label htmlFor=''>Nome do Procedimento:</label>
+                                <input type='text' {...register("proced1")} />
+                                <label htmlFor='' >Quantidade:</label>
+                                <input type='text' {...register("qtd1")} />
+                            </div>
+                            <div className="formField__divider"></div>
+                            {tussField1 && inputTuss(2)}
+                            <div className="formField__divider"></div>
+                            {tussField2 && inputTuss(3)}
+                            <div className="formField__divider"></div>
+                            {tussField3 && inputTuss(4)}
+                            <div className="formField__divider"></div>
+                            {tussField4 && inputTuss(5)}
+
+                            <label htmlFor=''>Adicionar Procedimento:</label>
+                            <button onClick={addTuss} type='button'>Adicionar</button>
+                            <button onClick={removeTuss} type='button'>Remover</button>
+                        </fieldset>
+                        {/* <button onClick={() => pageActive('guia')}>gerar</button> */}
+                        <button type="submit">Gerar Guia</button>
+                        <button type="button" onClick={() => reset()}>Limpar</button>
+                    </form>
+                }
+                {guiaActive && typeGuia === 'sadt' &&
+                    <div className="papel__sadt">
+                        <GuiaSadt data={dataGuia} func={{ setGuiaActive }} />
+                    </div>
+                }
+
+                {guiaActive && typeGuia === 'int' &&
                     <div className="papel">
                         <Guia data={dataGuia} func={{ setGuiaActive }} />
                     </div>
